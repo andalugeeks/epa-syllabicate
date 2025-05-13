1861ef2eae1d52f9d334907f144cf9249295c144
Sergio Soto <ssoto@freepik.com>
2025-05-14 00:18:13 +0200

    test: splits tests and applies black

--- tests/test_syllabicate.py
+++ tests/test_syllabicate.py
@@ -5,12 +5,22 @@ Tests para la función syllabicate
 import unittest
 from epa_syllabicate import syllabicate
 
+
 class TestSyllabicate(unittest.TestCase):
     def test_basic_word(self):
         """Test para palabras básicas"""
         self.assertEqual(syllabicate("toque"), ["to", "que"])
+
+    def test_casa_word(self):
+        """Test para palabras con qu"""
         self.assertEqual(syllabicate("casa"), ["ca", "sa"])
+
+    def test_meça_word(self):
+        """Test para palabras con ç"""
         self.assertEqual(syllabicate("meça"), ["me", "ça"])
+
+    def test_çofá_word(self):
+        """Test para palabras con ç"""
         self.assertEqual(syllabicate("çofá"), ["ço", "fá"])
 
     def test_empty_word(self):
@@ -19,17 +29,31 @@ class TestSyllabicate(unittest.TestCase):
 
     def test_single_letter(self):
         """Test para palabras de una letra"""
-        self.assertEqual(syllabicate("a"), ["a"])
-        self.assertEqual(syllabicate("e"), ["e"])
+
+    def test_e_word(self):
+        """Test para palabras con ae"""
+        self.assertEqual(syllabicate("ê"), ["ê"])
+
+    def test_a_word(self):
+        """Test para palabras con ae"""
         self.assertEqual(syllabicate("â"), ["â"])
 
-    def test_complex_words(self):
+    def test_complex_words_1(self):
         """Test para palabras más complejas"""
         self.assertEqual(syllabicate("çôh"), ["çôh"])
+
+    def test_complex_words_2(self):
+        """Test para palabras más complejas"""
         self.assertEqual(syllabicate("êttanda"), ["ê", "ttan", "da"])
+
+    def test_complex_words_3(self):
+        """Test para palabras más complejas"""
         self.assertEqual(syllabicate("cohêl-lo"), ["co", "hêl", "lo"])
+
+    def test_complex_words_4(self):
+        """Test para palabras más complejas"""
         self.assertEqual(syllabicate("ôhhetibâh"), ["ô", "hhe", "ti", "bâh"])
-      
+
 
 if __name__ == "__main__":
     unittest.main()
