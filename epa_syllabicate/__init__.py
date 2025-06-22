9aeee5728f8560797861695d40497e888d83d30d
Sergio Soto <ssoto@freepik.com>
2025-06-23 00:28:00 +0200

    feat: integrate klow-e syllabicate with corpus tests

--- epa_syllabicate/__init__.py
+++ epa_syllabicate/__init__.py
@@ -4,6 +4,6 @@ EPA Syllabicate - Módulo para silabificación de palabras
 
 __version__ = "0.1.0"
 
-from .syllabicate import syllabicate
+from .syllabicator import syllabify
 
 __all__ = ["syllabicate"]
